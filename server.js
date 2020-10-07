66580637092b0583235a38f9218b46aa16d9f279
Marcelino Niz <mniz@uncc.edu>
2020-10-07 13:03:48 -0400

    installed and implemented cors

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
+
+
 
 const data = require("./data");
 const budget = {
@@ -22,14 +27,12 @@ const budget = {
   ]
 };
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+
 
 app.get('/budget', (req, res) => {
     res.json(data);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log('Api served at http://localhost:${port}');
 });
